48d3f10894b7cfc35e57d3dafbe61ffc174096c3
Nathan Park <nalhan.park@gmail.com>
2026-02-12 19:04:04 -0800

    feat: add bota LSM sounds and textures

---
+++ LSMRegister.lua
@@ -0,0 +1,60 @@
+local _, BOTA = ...
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- -----
+-- BACKGROUND
+-- -----
+LSM:Register("background", "sprite", "Interface\\Addons\\BotaTools\\background\\sprite.tga")
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "Expressway", "Interface\\Addons\\BotaTools\\font\\Expressway.ttf")
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "crisp_soda", "Interface\\Addons\\BotaTools\\sound\\crisp_soda.ogg")
+LSM:Register("sound", "crunchy_soda", "Interface\\Addons\\BotaTools\\sound\\crunchy_soda.ogg")
+LSM:Register("sound", "crush_soda", "Interface\\Addons\\BotaTools\\sound\\crush_soda.ogg")
+LSM:Register("sound", "se.1", "Interface\\Addons\\BotaTools\\sound\\se.1.mp3")
+LSM:Register("sound", "se.10", "Interface\\Addons\\BotaTools\\sound\\se.10.mp3")
+LSM:Register("sound", "se.11", "Interface\\Addons\\BotaTools\\sound\\se.11.mp3")
+LSM:Register("sound", "se.12", "Interface\\Addons\\BotaTools\\sound\\se.12.mp3")
+LSM:Register("sound", "se.13", "Interface\\Addons\\BotaTools\\sound\\se.13.mp3")
+LSM:Register("sound", "se.14", "Interface\\Addons\\BotaTools\\sound\\se.14.mp3")
+LSM:Register("sound", "se.15", "Interface\\Addons\\BotaTools\\sound\\se.15.mp3")
+LSM:Register("sound", "se.16", "Interface\\Addons\\BotaTools\\sound\\se.16.mp3")
+LSM:Register("sound", "se.2", "Interface\\Addons\\BotaTools\\sound\\se.2.mp3")
+LSM:Register("sound", "se.3", "Interface\\Addons\\BotaTools\\sound\\se.3.mp3")
+LSM:Register("sound", "se.4", "Interface\\Addons\\BotaTools\\sound\\se.4.mp3")
+LSM:Register("sound", "se.5", "Interface\\Addons\\BotaTools\\sound\\se.5.mp3")
+LSM:Register("sound", "se.6", "Interface\\Addons\\BotaTools\\sound\\se.6.mp3")
+LSM:Register("sound", "se.7", "Interface\\Addons\\BotaTools\\sound\\se.7.mp3")
+LSM:Register("sound", "se.8", "Interface\\Addons\\BotaTools\\sound\\se.8.mp3")
+LSM:Register("sound", "se.9", "Interface\\Addons\\BotaTools\\sound\\se.9.mp3")
+LSM:Register("sound", "soda_pouring", "Interface\\Addons\\BotaTools\\sound\\soda_pouring.ogg")
+LSM:Register("sound", "soda_pouring_2", "Interface\\Addons\\BotaTools\\sound\\soda_pouring_2.ogg")
+LSM:Register("sound", "soda_pour_short", "Interface\\Addons\\BotaTools\\sound\\soda_pour_short.ogg")
+LSM:Register("sound", "soda_pour_short_2", "Interface\\Addons\\BotaTools\\sound\\soda_pour_short_2.ogg")
+LSM:Register("sound", "soda_pour_short_3", "Interface\\Addons\\BotaTools\\sound\\soda_pour_short_3.ogg")
+LSM:Register("sound", "soda_sipping", "Interface\\Addons\\BotaTools\\sound\\soda_sipping.ogg")
+LSM:Register("sound", "soda_slurping", "Interface\\Addons\\BotaTools\\sound\\soda_slurping.ogg")
+LSM:Register("sound", "tim-allens-home-improvement-grunt",
+    "Interface\\Addons\\BotaTools\\sound\\tim-allens-home-improvement-grunt.ogg")
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "Pixel", "Interface\\Addons\\BotaTools\\statusbar\\Pixel.tga")
+LSM:Register("statusbar", "Skullflower", "Interface\\Addons\\BotaTools\\statusbar\\Skullflower.tga")
+LSM:Register("statusbar", "Skullflower1", "Interface\\Addons\\BotaTools\\statusbar\\Skullflower1.tga")
+LSM:Register("statusbar", "Skullflower2", "Interface\\Addons\\BotaTools\\statusbar\\Skullflower2.tga")
+LSM:Register("statusbar", "Skullflower3", "Interface\\Addons\\BotaTools\\statusbar\\Skullflower3.tga")
+LSM:Register("statusbar", "Skullflower4", "Interface\\Addons\\BotaTools\\statusbar\\Skullflower4.tga")
+LSM:Register("statusbar", "Skullflower5", "Interface\\Addons\\BotaTools\\statusbar\\Skullflower5.tga")
+LSM:Register("statusbar", "SkullflowerLight", "Interface\\Addons\\BotaTools\\statusbar\\SkullflowerLight.tga")
